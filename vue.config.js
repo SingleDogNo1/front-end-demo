aaad8523640183d3a3ab63a3a9dfed93cea8580c
lost-dream <13623673118@163.com>
2019-12-03 14:45:40 +0800

    111

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const CompressionPlugin = require('compression-webpack-plugin')
 const MockData = require('./mockData/mock')
 const env = process.env
